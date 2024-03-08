5749eb4cbca2aab1bd17e0988e120efe751b39b0
zena-kebede <151683487+zena-kebede@users.noreply.github.com>
2024-03-08 15:20:32 -0700

    Update Reservation.cs
    Inline comments added

--- Reservation.cs
+++ Reservation.cs
@@ -8,8 +8,14 @@ namespace Assignment2.Components.Pages.Data
 {
     internal class Reservation
     {
+        ///
+        ///Represents a reservation for a flight.
+        ///
+
+        //Constrant that is representing the size of each reservation record.
         public const int RECORD_SIZE = 201;
 
+        // Private fields representing reservation attributes
         private string code;
         private string flightCode;
         private string airline;
@@ -19,9 +25,14 @@ namespace Assignment2.Components.Pages.Data
         private string active;
         private string reservationCode;
         private Flight flight;
+
         public Reservation()
         {
         }
+
+        ///
+        /// Constructor for initializing a reservation with basic information
+        ///        
         public Reservation(string code, string flightCode, string airline, double cost, string name, string citizenship, string active)
         {
             this.code = code;
@@ -33,6 +44,7 @@ namespace Assignment2.Components.Pages.Data
             this.active = active;
         }
 
+        //Constructor for intializing a reservation with flight and passenger information incl.  
         public Reservation(string reservationCode, Flight flight, string name, string citizenship)
         {
             this.reservationCode = reservationCode;
@@ -41,6 +53,7 @@ namespace Assignment2.Components.Pages.Data
             this.citizenship = citizenship;
         }
 
+        // Accessing private fields
         public string Code { get => code; set => code = value; }
         public string FlightCode { get => flightCode; set => flightCode = value; }
         public string Airline { get => airline; set => airline = value; }
@@ -49,15 +62,18 @@ namespace Assignment2.Components.Pages.Data
         public double Cost { get => cost; set => cost = value; }
         public string Active { get => active; set => active = value; }
 
+        //Sets the name of the passenger
         public void SetName(string name)// throws InvalidNameException
         {
             if (string.IsNullOrEmpty(name))
             {
-                throw new ArgumentNullException("name");
+                throw new ArgumentNullException("name"); //thrown when the passenger name is NULL or empty.
             }
 
             this.name = name;
         }
+
+        //Sets the citizenship of the passenger.
         public void setCitizenship(string citizenship) //throws InvalidCitizenshipException
         {
             if (string.IsNullOrEmpty(citizenship))
@@ -68,6 +84,7 @@ namespace Assignment2.Components.Pages.Data
             this.citizenship = citizenship;
         }
 
+        //Return a string version of the reservation.
         public string toString()
         {
             // return this.Code;
